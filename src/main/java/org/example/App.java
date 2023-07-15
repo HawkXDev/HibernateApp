671f16274a0b9dd99bdf2c3213f9b92c8c27a420
sokolserg <sergsokolkin@gmail.com>
2023-07-15 10:07:35 +0300

    Lazy loading, Lazy/Eager, Hibernate.initialize(proxy), createQuery

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,61 +1,61 @@
 package org.example;
 
 import org.example.model.*;
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
         Configuration configuration = new Configuration()
-                .addAnnotatedClass(Actor.class)
-                .addAnnotatedClass(Movie.class);
+                .addAnnotatedClass(Person.class)
+                .addAnnotatedClass(Item.class)
+                .addAnnotatedClass(Passport.class);
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.getCurrentSession()) {
 
             session.beginTransaction();
 
-//            Movie movie = new Movie("Pulp Fiction", 1994);
-//            Actor actor1 = new Actor("Harvey Keitel", 81);
-//            Actor actor2 = new Actor("Samuel L. Jackson", 72);
-//
-//            movie.setActors(new ArrayList<>(List.of(actor1, actor2)));
-//
-//            actor1.setMovies(new ArrayList<>(Collections.singletonList(movie)));
-//            actor2.setMovies(new ArrayList<>(Collections.singletonList(movie)));
-//
-//            session.persist(movie);
-//            session.persist(actor1);
-//            session.persist(actor2);
+            Person person = session.get(Person.class, 1);
+            System.out.println("Get person");
 
-//            Movie movie = session.get(Movie.class, 1);
-//            System.out.println(movie.getActors());
-//
-//            Actor actor = session.get(Actor.class, 1);
-//            System.out.println(actor.getMovies());
+            // Get related entities (Lazy)
+            System.out.println(person.getItems());
 
-//            Movie movie = new Movie("Reservoir Dogs", 1992);
-//            Actor actor = session.get(Actor.class, 1);
-//
-//            movie.setActors(new ArrayList<>(Collections.singletonList(actor)));
-//            actor.getMovies().add(movie);
+            Item item = session.get(Item.class, 1);
+            System.out.println("Get item");
 
-//            session.persist(movie);
+            // Get related owner (Eager)
+            System.out.println(item.getOwner());
 
-            Actor actor = session.get(Actor.class, 2);
-            System.out.println(actor.getMovies());
+            Person person1 = session.get(Person.class, 10);
 
-            Movie movieToRemove = actor.getMovies().get(0);
-
-            actor.getMovies().remove(movieToRemove);
-            movieToRemove.getActors().remove(actor);
+            Person person2 = session.get(Person.class, 4);
+            Hibernate.initialize(person2.getItems());
 
             session.getTransaction().commit();
+
+//            # LazyInitializationException
+//            System.out.println(person1.getItems());
+
+            System.out.println(person2.getItems());
+
+            // Open the session and transaction again
+            Session session1 = sessionFactory.getCurrentSession();
+            session1.beginTransaction();
+
+            System.out.println("Inside the second transaction");
+
+            List<Item> items = session1.createQuery("select i from Item i where i.owner.id=:personId",
+                            Item.class).setParameter("personId", person1.getId()).list();
+
+            session1.getTransaction().commit();
+
+            System.out.println(items);
         }
     }
 }
